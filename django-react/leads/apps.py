cf2c310dacaa5035a9ac66763bee429608092fe1
KyrahWiggins <kyrah.wiggins@gmail.com>
2021-07-27 16:04:36 -0400

    build

--- django-react/leads/apps.py
+++ django-react/leads/apps.py
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.apps import AppConfig
 
 
 class LeadsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'leads'
+
--- django-react/leads/views.py
+++ django-react/leads/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from .models import Lead
 from .serializers import LeadSerializer
 from rest_framework import generics
+
 
 class LeadListCreate(generics.ListCreateAPIView):
     queryset = Lead.objects.all()
